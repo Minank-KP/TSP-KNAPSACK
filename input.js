a955188b2c46a1656db07ddb995c3ef0abe9aa3f
Minank-KP <minankkp2019@gmail.com>
2023-05-22 23:53:45 +0530

    input and matrix done

--- input.js
+++ input.js
@@ -1,3 +1,15 @@
+class Node {
+    constructor(x,y) {
+        this.x=x;
+        this.y=y;
+        this.time=Math.floor(Math.random() * 10) + 1;
+        this.enj=Math.floor(Math.random() * 10) + 1;
+    }
+}
+
+const nodes =[]
+
+
 /**@type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -13,9 +25,43 @@ canvas.tabindex = 1;
 canvas.addEventListener("click", function(event){
     let x = event.offsetX;
     let y = event.offsetY;
+    let node = new Node(x,y);
+    nodes.push(node)
     ctx.fillStyle = "red";
     ctx.beginPath();
     ctx.arc(x, y, 5, 0, Math.PI*2);
     ctx.fill();
     ctx.stroke();
+    console.log(nodes)
 });
+
+const Time_arr = []
+function generateMatrix() {
+
+    for (var i = 0; i < nodes.length(); i++) {
+    var row = [];
+    for (var j = 0; j < nodes.length(); j++) {
+        if(i==j)
+            row.push(0);
+        else{
+            var dx=nodes[i].x-nodes[j].x;
+            var dy=nodes[i].y-nodes[j].y;
+            var diff = Math.sqrt(dx*dx+dy*dy);
+            row.push(diff+nodes[i].time);
+        }   
+    }
+    Time_arr.push(row);
+    }
+}
+
+
+
+function calculateOutput() {
+    generateMatrix();
+}
+
+
+document.getElementById("calculate").onclick = () => {
+    calculateOutput();
+  };
+
